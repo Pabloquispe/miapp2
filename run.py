86691dc8ea6bb139212403135569d89ed655840a
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-06-26 11:40:21 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import os
 from app import create_app
 
+config_name = os.getenv('FLASK_CONFIG') or 'default'
+app = create_app(config_name)
+
 if __name__ == "__main__":
-    config_name = os.getenv('FLASK_CONFIG') or 'default'
-    app = create_app(config_name)
     app.run(debug=(config_name == 'dev'))
