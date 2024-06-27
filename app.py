04c298f4cc44a82c0e3172e851846c5805183404
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-06-27 07:03:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def create_app(config_name):
     """Crea y configura la aplicación Flask."""
     app = Flask(__name__, template_folder='vistas/templates', static_folder='vistas/static')
     app.config.from_object(config_by_name[config_name])
-    
+
     # Inicializar la base de datos
     db.init_app(app)
     migrate = Migrate(app, db)
@@ -21,10 +21,16 @@ def create_app(config_name):
     app.register_blueprint(user_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
-    
+
     with app.app_context():
         from controladores.routes import register_routes
         register_routes(app)
         db.create_all()
 
     return app
+
+if __name__ == "__main__":
+    config_name = os.getenv('FLASK_CONFIG') or 'default'
+    app = create_app(config_name)
+    app.run(debug=(config_name == 'dev'))
+
