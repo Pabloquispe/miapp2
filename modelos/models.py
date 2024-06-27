cb07f78f06d22c20bb7adc5fb376754c25010f0f
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-06-27 17:00:51 -0500

    Update models.py

--- modelos/models.py
+++ modelos/models.py
@@ -1,6 +1,5 @@
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime 
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
