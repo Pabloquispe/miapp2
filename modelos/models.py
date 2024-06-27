fbc8cee985f26755b1df6668a0b1e151643eda9c
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-06-27 15:09:12 -0500

    Update models.py

--- modelos/models.py
+++ modelos/models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 
 
 class Usuario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(100), nullable=False)
     apellido = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
