107a7d1fda9003050d7591a0c978d38071c50c39
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-06-28 17:09:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,10 @@
-import os
 from flask import Flask
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from config import config_by_name
-from controladores.admin_routes import admin_bp
-from controladores.user_routes import user_bp
-from controladores.auth_routes import auth_bp
-from controladores.main_routes import main_bp
 
-# Inicializa la base de datos
-db = SQLAlchemy()
+app = Flask(__name__)
 
-def create_app(config_name):
-    """Crea y configura la aplicación Flask."""
-    app = Flask(__name__, template_folder='vistas/templates', static_folder='vistas/static')
-    app.config.from_object(config_by_name[config_name])
-
-    # Inicializar la base de datos
-    db.init_app(app)
-    migrate = Migrate(app, db)
-
-    # Registrar Blueprints
-    app.register_blueprint(admin_bp)
-    app.register_blueprint(user_bp)
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(main_bp)
-
-    with app.app_context():
-        from controladores.routes import register_routes
-        register_routes(app)
-        db.create_all()
-
-    return app
-
-if __name__ == "__main__":
-    config_name = os.getenv('FLASK_CONFIG') or 'default'
-    app = create_app(config_name)
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=(config_name == 'dev'))
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
 
 
