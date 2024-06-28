5492a93583b69a6301e39e94820d8c279d4d8982
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-06-28 17:27:19 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,2 @@
-import os
-from app import create_app
 
-config_name = os.getenv('FLASK_CONFIG') or 'default'
-app = create_app(config_name)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=(config_name == 'dev'))
 
